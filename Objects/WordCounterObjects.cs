683a846ce9bbf95894db0cedfab64bd92734af35
ian Wilcox <ian Wilcox>
2016-09-16 11:48:03 -0700

    add Get and Set for UserWords

--- Objects/WordCounterObjects.cs
+++ Objects/WordCounterObjects.cs
@@ -6,5 +6,30 @@ namespace WordCounter.Objects
 {
   public class UserWords
   {
+    private string _inputWord;
+    private string _inputSentence;
+
+    public UserWords(string inputWord, string inputSentence)
+    {
+      _inputWord = inputWord;
+      _inputSentence = inputSentence;
+    }
+    public string GetInputWord()
+    {
+      return _inputWord;
+    }
+    public void SetInputWord(string inputWord)
+    {
+      _inputWord = inputWord;
+    }
+    public string GetInputSentence()
+    {
+      return _inputSentence;
+    }
+    public void SetInputWord(string inputSentence)
+    {
+      _inputSentence = inputSentence;
+    }
+
 }
 }
